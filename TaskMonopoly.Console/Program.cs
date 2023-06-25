186bc01e467713236425b46312000609ba0d2cc6
Sklyarenko Philipp <filipp.sklyarenko@mail.ru>
2023-06-25 20:21:34 +0300

    Добавил задержку перед завершением программы

--- TaskMonopoly.Console/Program.cs
+++ TaskMonopoly.Console/Program.cs
@@ -37,6 +37,10 @@ try
     ShowResult_2InConsole(result_2);
 
     Task.WaitAll(writeResult_1Task, writeResult_2Task);
+
+    Console.WriteLine();
+    Console.WriteLine("Press any key to close this window . . .");
+    Console.ReadKey();
 }
 catch (Exception ex)
 {
